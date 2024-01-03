31959a283e5286f93ce3c7bddbe6e25ad78d3e4b
Wallacewss2033 <wallace-2033@hotmail.com>
2024-01-03 10:46:22 -0300

    create: componente de formulário

---
+++ onfly-web/src/components/FormView.vue
@@ -0,0 +1,67 @@
+<template>
+  <form @submit.stop.prevent="submit" class="row g-3">
+    <div class="col-md-6">
+      <input
+        ref="meuInput"
+        v-model="data.description"
+        type="text"
+        class="form-control"
+        placeholder="Descrição"
+        aria-label="Descrição"
+      />
+    </div>
+    <div class="col-md-6">
+      <input
+        v-model="data.date"
+        type="date"
+        class="form-control"
+        aria-label="Last name"
+      />
+    </div>
+    <div class="col-md-6">
+      <input
+        v-model="data.value"
+        type="text"
+        class="form-control"
+        placeholder="Valor"
+        aria-label="Valor"
+      />
+    </div>
+    <div class="col-md-12">
+      <button type="submit" class="d-flex btn btn-primary">criar</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "FormView",
+  props: {
+    expense: {
+      type: Object,
+      required: false,
+    },
+  },
+  watch: {
+    expense(newData) {
+      if (newData) {
+        this.data = { ...newData };
+      }
+    },
+  },
+  data() {
+    return {
+      data: {
+        description: "",
+        date: "",
+        value: null,
+      },
+    };
+  },
+  methods: {
+    submit() {
+      this.$emit("handler-submit", JSON.stringify(this.data));
+    },
+  },
+};
+</script>
