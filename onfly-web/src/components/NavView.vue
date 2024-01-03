89bf259126c709dd6d11f3ef302329058c9b26d6
Wallacewss2033 <wallace-2033@hotmail.com>
2024-01-03 10:46:32 -0300

    create nav

---
+++ onfly-web/src/components/NavView.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav v-if="!isLoginPage">
+    <router-link to="/home">Home</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavView",
+  data() {
+    return {
+      isLoginPage: false,
+    };
+  },
+  watch: {
+    $route(to) {
+      this.isLoginPage = to.name === "login";
+    },
+  },
+};
+</script>
